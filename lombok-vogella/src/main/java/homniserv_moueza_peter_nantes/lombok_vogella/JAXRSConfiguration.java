9f8cc48b5f08124ad0965cd5ccb68fa35b29f7c8
moueza <peter@oscillan1>
2020-10-24 09:43:08 +0200

    cleaning

--- lombok-vogella/src/main/java/homniserv_moueza_peter_nantes/lombok_vogella/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package homniserv_moueza_peter_nantes.lombok_vogella;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
